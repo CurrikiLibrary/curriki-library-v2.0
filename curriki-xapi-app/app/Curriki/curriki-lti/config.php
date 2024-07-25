477ed5c939aaf249c422302c1be7fb39899be8c4
Waqar Muneer <i.do.dev@outlook.com>
2024-07-25 18:35:04 +0500

    sub project update

---
+++ curriki-xapi-app/app/Curriki/curriki-lti/config.php
@@ -0,0 +1,21 @@
+<?php
+define('LTI_LAUNCH_CONTAINER_DEFAULT', 1);
+define('LTI_LAUNCH_CONTAINER_EMBED', 2);
+define('LTI_LAUNCH_CONTAINER_EMBED_NO_BLOCKS', 3);
+define('LTI_LAUNCH_CONTAINER_WINDOW', 4);
+define('LTI_LAUNCH_CONTAINER_REPLACE_MOODLE_WINDOW', 5);
+define('LTI_VERSION_1P3', '1.3.0');
+define('LTI_TOOL_STATE_CONFIGURED', 1);
+define('LTI_URL_DOMAIN_REGEX', '/(?:https?:\/\/)?(?:www\.)?([^\/]+)(?:\/|$)/i');
+define('LTI_SETTING_NEVER', 0);
+define('LTI_SETTING_ALWAYS', 1);
+define('LTI_SETTING_DELEGATE', 2);
+
+define('WP_LTI_APP_URL', '');
+define('WP_LTI_DB_DRIVER', '');
+define('WP_LTI_DB_HOST', '');
+define('WP_LTI_DB_USER', '');
+define('WP_LTI_DB_PASSWORD', '');
+define('WP_LTI_DB_NAME', '');
+define('WP_LTI_DB_TABLE_PREFIX', '');
+
